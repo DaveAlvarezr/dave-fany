ead3e501ff9b7077a98c1caa693f3477de129311
Fany Martinez <estefania.marhen@gmail.com>
2023-05-25 17:21:12 +0100

    format code

--- katas/01-hello-world/kata.js
+++ katas/01-hello-world/kata.js
@@ -1,9 +1,9 @@
 export function kata() {
-const div = document.createElement("div")
-div.id = "square" 
-const p = document.createElement("p")
-p.id = "text"
-p.textContent = "Hello World"
-div.appendChild(p)
-document.body.appendChild(div)
+  const div = document.createElement("div");
+  div.id = "square";
+  const p = document.createElement("p");
+  p.id = "text";
+  p.textContent = "Hello World";
+  div.appendChild(p);
+  document.body.appendChild(div);
 }
