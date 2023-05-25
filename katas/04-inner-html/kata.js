cf737dff320c391dfb16ee9a85649e2a9ca5e680
Daniel Ramos <danielramosacosta1@gmail.com>
2023-05-25 20:55:24 +0100

    finish refactor

--- katas/04-inner-html/kata.js
+++ katas/04-inner-html/kata.js
@@ -1,42 +1,43 @@
-const article1 = `<article>
-            <img src="https://picsum.photos/id/28/800/300">
-            <div class="content">
-                <p class="category">travel</p>
-                <h2>Trip Planning for Total Beginners</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt
-                    repellendus
-                    dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus
-                    eligendi
-                    neque, qui quas esse.</p>
-            </div>
-        </article>`
-const article2 = `<article>
-            <img src="https://picsum.photos/id/35/800/300">
-            <div class="content">
-                <p class="category">travel</p>
-                <h2>7 ways to survive in the desert</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt
-                    repellendus
-                    dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus
-                    eligendi
-                    neque, qui quas esse.</p>
-            </div>
-        </article>`
-const article3 = `<article>
-            <img src="https://picsum.photos/id/37/800/300">
-            <div class="content">
-                <p class="category">travel</p>
-                <h2>5 amazing travel destinations</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt
-                    repellendus
-                    dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus
-                    eligendi
-                    neque, qui quas esse.</p>
-            </div>
-        </article>`
-const articles = [article1, article2, article3].join("\n")
 export function kata() {
-  document.body.innerHTML = `<main>
-    ${articles}
-    </main>`;
+  const destinations = [
+    {
+      title: "Trip Planning for Total Beginners",
+      category: "travel",
+      image: "https://picsum.photos/id/28/800/300",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt repellendus dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus eligendi neque, qui quas esse."
+    },
+    {
+      title: "7 ways to survive in the desert",
+      category: "travel",
+      image: "https://picsum.photos/id/35/800/300",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt repellendus dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus eligendi neque, qui quas esse."
+    },
+    {
+      title: "5 amazing travel destinations",
+      category: "travel",
+      image: "https://picsum.photos/id/37/800/300",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet reprehenderit nesciunt repellendus dolorem ipsa eaque repudiandae odit aperiam est assumenda, impedit alias doloremque temporibus eligendi neque, qui quas esse."
+    }
+  ]
+
+  document.body.innerHTML = renderMain(destinations.map(renderArticle).join("\n"));
+}
+
+function renderMain(articles) {
+  return `<main>${articles}</main>`;
+}
+
+function renderArticle({image, category, title, content}) {
+  return `<article>
+            <img src="${image}">
+            <div class="content">
+                ${(renderContent(category, title, content))}
+            </div>
+        </article>`;
+}
+
+function renderContent(category, title, content) {
+  return `<p class="category">${category}</p>
+          <h2>${title}</h2>
+          <p>${content}</p>`;
 }
